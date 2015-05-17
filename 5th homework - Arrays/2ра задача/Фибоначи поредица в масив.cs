896170ee40c06d75ea9dcd6ab9c60cc9428576b4
dddilian <dilian.dimitrov85@gmail.com>
2015-05-17 12:32:02 +0300

    5th homework - Arrays

---
+++ 5th homework - Arrays/2ра задача/Фибоначи поредица в масив.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ра_задача
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for array length:");
+            int n = int.Parse(Console.ReadLine());
+            int[] Array = new int[n];
+
+            
+            Array[0] = 0; // определят се първите два елемента да са 0 и 1, 
+            Array[1] = 1; // за да бъдат основа при пълненето на масива в определената последователност
+
+            Console.WriteLine(Array[0]);
+            Console.WriteLine(Array[1]);
+            for (int i = 2; i < n; i++) //запълването на масива започва от 2рия индекс, т.е. 3ти елемент.
+            {
+                Array[i] = Array[i - 1] + Array[i - 2];
+                
+                Console.WriteLine(Array[i]);
+            }
+        }
+    }
+}
+
---
+++ 5th homework - Arrays/ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 30;
+            int b = 1;
+
+            Console.WriteLine("The numbers below are divided by 3 without a remainder");
+            for (int i = a; a >= b; a--)
+            {
+                Console.WriteLine(a);
+                
+            }
+        }
+    }
+}
